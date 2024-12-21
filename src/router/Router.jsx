208711f94b98f1b84fa094541d06f1558048fd0f
bintissa24434 <bintissa24434@gmail.com>
2024-12-21 01:41:22 +0000

    Fix imports and add 404 handling in Router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,21 +1,20 @@
-import {Routes, Route} from 'react-router'
-import Home from '../pages/Home'
-import Reviews from '../pages/Reviews'
-import MainLayout from '../layouts/MainLayout'
-import Services from '../pages/Services'
-
+import { Routes, Route } from 'react-router-dom'; // Correct import
+import Home from '../pages/Home';
+import Reviews from '../pages/Reviews';
+import MainLayout from '../layouts/MainLayout';
+import Services from '../pages/Services';
 
 function Router() {
     return (
         <Routes>
             <Route element={<MainLayout />}>
-                <Route path="/"element={<Home />} />
-                <Route path="/services"element={<Services />} />
-                <Route path="/reviews"element={<Reviews />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/reviews" element={<Reviews />} />
             </Route>
+            <Route path="*" element={<div>Page Not Found</div>} />
         </Routes>
-    )
+    );
 }
 
-export default Router
-
+export default Router;
